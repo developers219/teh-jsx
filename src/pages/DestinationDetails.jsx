492cddffb49d664534084ad2c4049216813b4b65
shiv-anya <sg90883@gmail.com>
2026-09-07 17:58:19 +0530

    made reviews section responsive

--- src/pages/DestinationDetails.jsx
+++ src/pages/DestinationDetails.jsx
@@ -106,25 +106,25 @@ export default function DestinationDetails() {
         }
       />
       <PackageByCategories
-  heading={"Romantic Getaways"}
-  subheading={
-    "Create unforgettable moments together with handpicked romantic escapes, perfect for honeymoons, anniversaries, and couples looking for a special getaway."
-  }
-/>
+        heading={"Romantic Getaways"}
+        subheading={
+          "Create unforgettable moments together with handpicked romantic escapes, perfect for honeymoons, anniversaries, and couples looking for a special getaway."
+        }
+      />
 
-<PackageByCategories
-  heading={"Group Adventures"}
-  subheading={
-    "Travel together, create lasting memories, and enjoy exciting group experiences with carefully curated packages designed for friends, families, and groups."
-  }
-/>
+      <PackageByCategories
+        heading={"Group Adventures"}
+        subheading={
+          "Travel together, create lasting memories, and enjoy exciting group experiences with carefully curated packages designed for friends, families, and groups."
+        }
+      />
 
-<PackageByCategories
-  heading={"Solo Adventures"}
-  subheading={
-    "Discover the freedom to travel your way with curated solo experiences, exciting destinations, and unforgettable adventures made for independent explorers."
-  }
-/>
+      <PackageByCategories
+        heading={"Solo Adventures"}
+        subheading={
+          "Discover the freedom to travel your way with curated solo experiences, exciting destinations, and unforgettable adventures made for independent explorers."
+        }
+      />
     </>
   );
 }
