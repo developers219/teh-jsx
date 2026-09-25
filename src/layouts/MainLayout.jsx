1fb6bfc800df689ab8aee36b576803b8812f9bce
developers219 <developers@travelempires.com>
2026-09-25 02:49:36 -0400

    update leadgenerationlink

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,13 +3,15 @@ import Footer from "../components/Footer";
 import LeadCapturePopup from "../components/forms/LeadCapturePopup";
 import Navbar from "../components/Navbar";
 import FloatingContactWidget from "../components/ui/FloatingContactWidget";
+import LeadGenerationLinks from "../components/ui/LeadGenerationLinks";
 import { useState } from "react";
 function MainLayout() {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="min-h-screen bg-slate-50">
       <Navbar />
-      <LeadCapturePopup isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* <LeadCapturePopup isOpen={isOpen} setIsOpen={setIsOpen} /> */}
+      <LeadGenerationLinks isOpen={isOpen} setIsOpen={setIsOpen}/>
       <Outlet />
       <Footer />
       <FloatingContactWidget />
